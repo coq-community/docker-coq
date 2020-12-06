f530f6489fe0b0a176fdc6e748bf8449fa366538
Erik Martin-Dorel <erik@martin-dorel.org>
2020-12-06 02:56:05 +0100

    feat: Add admin.sh (helper functions to compute compatible OCaml versions)

---
+++ admin.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# Author: Erik Martin-Dorel, 2020
+# Summary: helper functions to compute compatible OCaml versions
+
+ocamls() { opam switch list-available ocaml-base-compiler | grep -v -e '#' | cut -d ' ' -f 2; }
+
+pred_ocaml_for_coqs() {
+    for v; do
+	printf "* Coq $v: "'`'
+	printf '%s' "$(opam show "coq.$v" -f depends: | grep '"ocaml"')"
+	printf '`\n'
+    done
+}
+
+list_ocaml_for_coqs() {
+    local indent='      '
+    # To change manually:
+    local render='true'
+    # local render='false'
+    # Note: we may post-process the output to merge "coq" items with same ocaml
+    for v; do
+	local several='true'
+	printf "* Coq $v: "
+	if [ "$v" = "8.4.6" ] || [ "$v" = "8.5.3" ] || [ "$v" = "8.6.1" ]; then
+	    minimal="4.02.3"
+	    several='false'
+	else
+	    versions=$(opam search ocaml-base-compiler --no-switch --columns=version -V --coinstallable-with="coq.$v" | grep -v -e '#')
+	    if [[ "$versions" =~ "4.05.0" ]]; then
+		minimal="4.05.0"
+	    else
+		minimal=$(head -n 1 <<<"$versions")
+	    fi
+	fi
+	[ "$render" = 'true' ] && printf "\n${indent}minimal: ['"
+	printf "$minimal"
+	[ "$render" = 'true' ] && printf "']\n"
+	[ "$render" = 'true' ] && printf "${indent}base: ['$minimal'"
+	if [ "$several" = 'true' ]; then
+	    if [[ "$versions" =~ "4.07.1" ]]; then
+		if [ "$render" = 'true' ]; then
+		    printf ", '4.07.1-flambda'"
+		else
+		    printf " 4.07.1-flambda"
+		fi
+	    fi
+	    minor=$(cut -d '.' -f 1-2 <<<"$versions" | sort -u -V | tail -n 2)
+	    lasttwo=$(for v in $minor; do grep -e "^${v//./\\.}.*\$" <<<"$versions" | tail -n 1; done)
+	    maybelasttwo=$(grep -v -e "^${minimal//./\\.}\$" -e '^4\.07$' <<<"$lasttwo")
+	    if [ "$render" = 'true' ]; then
+		printf '%s' "$maybelasttwo" | xargs printf ", '%s-flambda'"
+	    else
+		printf '%s' "$maybelasttwo" | xargs printf " %s-flambda"
+	    fi
+	fi
+	[ "$render" = 'true' ] && printf "]"
+	printf "\n"
+	[ "$render" = 'true' ] && printf "${indent}coq: ['${v}']\n"
+    done
+}
+
+# pred_ocaml_for_coqs 8.4.6 8.5.3 8.6.1 8.7.2 8.8.2 8.9.1 8.10.2 8.11.2 8.12.1 # 8.13-alpha
+# list_ocaml_for_coqs 8.13.dev 8.12.1 8.11.2 8.10.2 8.9.1 8.8.2 8.7.2 8.6.1 8.5.3 8.4.6
